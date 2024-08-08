5da051fc0d6c23654a68b531bb5ef6b571e6f3a5
Habibur Rahman Nayan <habiburrahmannayan66@gmail.com>
2024-08-08 23:00:40 +0600

    converted mongoose validation error

--- src/app/middlwares/globalErrorhandler.ts
+++ src/app/middlwares/globalErrorhandler.ts
@@ -1,17 +1,48 @@
+/* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+import { TErrorSources } from '../interface/error';
+import config from '../config';
+import { handleZodError } from '../errors/handleZodError';
+import handleValidationError from '../errors/handleValidationError';
 
+export const globalErrorhandler: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
+) => {
+  //setting default values
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
 
-export const globalErrorhandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode =  err.statusCode || 500;
-    const message = err.message || 'Something went wrong!';
-  
-    return res.status(statusCode).json({
-      success: false,
-      message,
-      error: err,
-    });
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
   }
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
+  });
+};
