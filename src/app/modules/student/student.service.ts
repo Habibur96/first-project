fe514855e78fa7caf23214937c30ebef8feb1eec
Habibur Rahman Nayan <habiburrahmannayan66@gmail.com>
2024-08-12 01:22:29 +0600

    pagination and field limiting added

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -24,8 +24,9 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
   });
 
   //Filterigng
-  const excluedFields = ['searchTerm', 'sort', 'limit'];
+  const excluedFields = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
   excluedFields.forEach((el) => delete queryObj[el]);
+  console.log({ query }, { queryObj });
 
   const filterquery = searchQuery
     .find(queryObj)
@@ -42,16 +43,29 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
     sort = query.sort as string;
   }
 
-  const sortQuery =  filterquery.sort(sort);
-
+  const sortQuery = filterquery.sort(sort);
+  let page = 1;
   let limit = 1;
+  let skip = 0;
   if (query.limit) {
-    limit = query.limit as number
+    limit = Number(query.limit);
   }
-  const limitQuery = await sortQuery.limit(limit)
-  
+  if (query.page) {
+    page = Number(query.page);
+    skip = (page - 1) * limit;
+  }
+
+  const paginateQuery = sortQuery.skip(skip);
+
+  const limitQuery = paginateQuery.limit(limit);
+  //field limiting
+  let fields = '-__v';
+  if (query.fields) {
+    fields = (query.fields as string).split(',').join(' ');
     
-    return limitQuery;
+  }
+  const fieldQuery = await limitQuery.select(fields);
+  return fieldQuery;
 };
 
 const updateStudentIntoDB = async (id: string, payload: Partial<TStudent>) => {
